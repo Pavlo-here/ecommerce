9b7823f693da69f2e3b109d235bee39d3c7a591e
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-08-29 19:02:50 +0300

    allowed_hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-4a06aw0hch4wvrt!mwtuz%1)(=2aip%fj7d%wb-7lokv=!49w2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
